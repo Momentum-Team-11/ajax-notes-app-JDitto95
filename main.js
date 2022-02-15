c211e012bcee902160c9e7dc3e0a00498cd345c3
Joseph Menditto <Powell0295@gmail.com>
2022-02-14 21:21:42 -0500

    working on it

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const url = 'http://localhost:3000/notes'
 const NOTES = document.getElementById('NOTES')
 const form = document.getElementById('form')
+const notesOutput = document.getElementById('notesOutput')
 
-
-form.addEventListener('submit', function()) {
+form.addEventListener('submit', function(event) {
     event.preventDefault()
     const todoText = document.querySelector('#todo-text').value
     fetch(url, {
@@ -13,12 +13,24 @@ form.addEventListener('submit', function()) {
             item: todoText,
         }),
     })
-}
+    .then((res) => res.json())
+    .then((data) =>{
 
-// function(){
-//     fetch(url)
-//     .then((res) => res.json())
-//     .then((data) =>{
-//         for (let)
-//     })
-// }
+    })
+})
+
+function listNote(){
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        for (let noteObj of data){
+            renderNoteItem(noteObj)
+        }
+    })
+}
+function renderNoteItem(noteObj) {
+    const itemEl = document.createElement('p')
+    itemEl.p = noteObj.id
+    itemEl.innerHTML = `${noteObj.item}<br>${noteObj.body}`
+    notesOutput.appendChild(itemEl)
+}
